432b9f412a29ed0a72e4953d918841e419c052d9
Ilia Ablamonov <ilia@flamefork.ru>
2010-01-04 02:13:02 +0400

    Handmade compilation

--- src/test/clojure/ru/flamefork/fleet/test.clj
+++ src/test/clojure/ru/flamefork/fleet/test.clj
@@ -1,6 +1,8 @@
 (ns ru.flamefork.fleet
-  (:use clojure.test)
-  (:use clojure.contrib.pprint))
+  (:use
+    [clojure.test]
+    [clojure.contrib.pprint]
+    [clojure.contrib.def]))
 
 (deftest basics
   (pprint (fleet
@@ -12,10 +14,13 @@
 </ul>")))
 
 (deftest handmade-compilation
+  (defvar f
+    (partial apply str))
+
   (println
     ((fn [post]
-      (apply str ["<p>" (apply str (post :body)) "</p>\n<ul>\n"
-                  (apply str (for [tag (post :tags)]
-                    (apply str ["<li>" (apply str (str tag)) "</li>\n"])))
+      (f ["<p>" (f (post :body)) "</p>\n<ul>\n"
+                  (f (for [tag (post :tags)]
+                    (f ["<li>" (f (str tag)) "</li>\n"])))
                   "</ul>"])
       ) {:body "Body" :tags ["tag1" "tag2" "tag3"]})))
