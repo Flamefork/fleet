b9fc8cffb9b4cbd804e496f7bb886e5634363719
Ilia Ablamonov <ilia@flamefork.ru>
2010-01-12 21:31:19 +0400

    Deprecation worked out

--- src/test/clojure/ru/flamefork/util/clj_string_test.clj
+++ src/test/clojure/ru/flamefork/util/clj_string_test.clj
@@ -7,5 +7,5 @@
 (deftest with-meta-test
   (let [asd (CljString. "test")
         asd-w-meta (with-meta asd {:asd 123})]
-    (is (= ^asd-w-meta {:asd 123}))
+    (is (= (meta asd-w-meta) {:asd 123}))
     (is (= asd-w-meta asd))))
