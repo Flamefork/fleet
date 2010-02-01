7ccf6843c0616d279b19f8b2a932dc3efc5cb749
Ilia Ablamonov <ilia@flamefork.ru>
2010-02-02 03:55:31 +0400

    Antlr dependency removed

--- src/test/clojure/fleet/parser_builder_test.clj
+++ src/test/clojure/fleet/parser_builder_test.clj
@@ -1,12 +1,7 @@
 (ns fleet.parser-builder-test
   (:use
     [clojure.test]
-    [fleet parser builder]))
-
-(deftest parse-test
-  (let [tpl (slurp "src/test/fleet/second/post_tpl.fleet")
-        ast (slurp "src/test/fleet/second/post_tpl.ast.clj")]
-    (is (= (parse tpl) (read-string ast)))))
+    [fleet builder]))
 
 (deftest build-test
   (let [ast (read-string (slurp "src/test/fleet/second/post_tpl.ast.clj"))
