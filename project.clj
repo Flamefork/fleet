87b32de67a52a4a49faa1de22254a1f503435624
Ilia Ablamonov <ilia@flamefork.ru>
2011-01-01 22:14:40 +0300

    Make it run with latest lein. Break compatibility with lein < 1.4 Version 0.9.4

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject
-  fleet "0.9.3"
+  fleet "0.9.4"
   :description "Templating System for Clojure"
   :url "http://github.com/Flamefork/fleet"
 
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :java-source-path [["src"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]
-                     [lein-javac "1.2.1-SNAPSHOT"]])
+  :java-source-path "src"
+  :dev-dependencies [[lein-clojars "0.6.0"]])
