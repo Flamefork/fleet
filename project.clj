8a1926bee4db98fb5da4eb48d36aaa1b2d5b6119
Ilia Ablamonov <ilia@flamefork.ru>
2014-11-10 12:36:54 +0400

    Minor additions to project metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject fleet "0.10.2"
   :description "Templating System for Clojure"
   :url "http://github.com/Flamefork/fleet"
+  :scm {:name "git"
+        :url "https://github.com/Flamefork/fleet"}
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
+  :signing {:gpg-key "ilia@flamefork.ru"}
 
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :java-source-paths ["src"])
