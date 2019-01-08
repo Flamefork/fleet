bab7276bfd0e14ed7b1982c127e861d042555778
m.vakulich@softteco.com <mvakulich@Mishas-MacBook-Pro.local>
2019-01-08 13:53:15 +0300

    correct build to match jdk toolkit 1.7

--- project.clj
+++ project.clj
@@ -9,4 +9,21 @@
 
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :global-vars {*warn-on-reflection* true}
-  :java-source-paths ["src"])
+  :java-source-paths ["src"]
+  :aot :all
+  :pom-plugins [[org.apache.maven.plugins/maven-toolchains-plugin "1.1"
+                 {:executions ([:execution
+                                [:goals
+                                 [:goal toolchain]]])
+                  :configuration ([:toolchains
+                                   [:jdk
+                                    [:version 1.7]
+                                    [:vendor "oracle"]]])}]
+                [com.theoryinpractise/clojure-maven-plugin "1.8.1"
+                 {:extensions "true"
+                  :executions ([:execution
+                                [:id "clojure-compile"]
+                                [:phase "compile"]
+                                [:configuration
+                                 [:sourceDirectories [:sourceDirectory "src"]]]
+                                [:goals [:goal "compile"]]])}]])
